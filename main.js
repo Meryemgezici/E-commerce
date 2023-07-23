35387c5c6b474a9af8cfc9c391cced4cfce9d598
Meryemgezici <gezicimeryem34@gmail.com>
2023-07-23 16:25:49 +0300

    Ürnler için tasrım yapıldı ve ürünler çekildi.

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
 // diğer dosyalardan geleneler
-import {renderCategories} from "./ui.js";
-
+import {renderCategories, renderProducts} from "./ui.js";
+// import  {renderProducts} from "./ui.js";
 // html'in yükleme anını izleme 
-document.addEventListener('DOMContentLoaded',fetchCategories);
+document.addEventListener('DOMContentLoaded',()=>{
+    fetchCategories();
+    fetchProducts();
+});
 
 
 const baseURL="https://api.escuelajs.co/api/v1"
 
+// categories dinamik olarak çekildi
 function fetchCategories(){
 
     fetch(`${baseURL}/categories`)
     .then((response)=>response.json())
     .then((data)=>renderCategories(data))
     .catch((error)=>console.log(error))
+
+}
+
+
+// Ürünleri dinamik olarak çekme
+
+async function fetchProducts(){
+    try {
+        const res=await fetch(`${baseURL}/products`);
+        const data=await res.json();
+        renderProducts(data);
+    } catch (error) {
+        console.log(error);
+    }
+   
 
 }
--- ui.js
+++ ui.js
@@ -1,11 +1,13 @@
 // Arayüz işlemleri bu dosyada yapıcaz
 
-const categoryList=document.querySelector(".categories");
+const categoryList = document.querySelector(".categories");
+const productList = document.querySelector(".products");
 
+// Kategorileri ekrana yazdırma
 export function renderCategories(categories) {
 
-    categories.forEach((category) => {
-        console.log(category);
+    categories.slice(0, 4).forEach((category) => {
+        // console.log(category);
         // html elementi oluşturma
         const categoryDiv = document.createElement("div");
         // Category divine class ekleme
@@ -20,6 +22,26 @@ export function renderCategories(categories) {
         // kategorileri categories divine ekleme
         categoryList.appendChild(categoryDiv);
 
+    });
+
+}
+
+// Ürünleri ekrana bastırma
+export function renderProducts(products) {
+    products.slice(0, 39).forEach((product) => {
+        console.log(product);
+        const productDiv = document.createElement("div");
+        productDiv.className = "product";
+        productDiv.innerHTML = `<img src="${product.images}"
+        alt="Ürün">
+            <p>${product.title}</p>
+            <p>${product.category.name}</p>
+            <div class="product-info">
+            <p>777 tl</p>
+            <button>Sepete ekle</button>
+         </div>`;
+
+         productList.appendChild(productDiv);
     });
 
 
