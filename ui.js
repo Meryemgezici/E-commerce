ee239dab76f3e9ba92b30df96ba2d0d381f76b62
Meryemgezici <gezicimeryem34@gmail.com>
2023-07-24 14:23:04 +0300

    Sepete ürün ekleme ve seppetten ürün silme işlemleri yapıldı.

--- ui.js
+++ ui.js
@@ -1,48 +1,82 @@
-// Arayüz işlemleri bu dosyada yapıcaz
+// Aaryüz işlemlerini bu dosyada yapıcaz
 
-const categoryList = document.querySelector(".categories");
-const productList = document.querySelector(".products");
+const categoryList = document.querySelector('.categories');
+const productList = document.querySelector('.products');
+const basketList = document.querySelector('.list');
 
-// Kategorileri ekrana yazdırma
 export function renderCategories(categories) {
-
-    categories.slice(0, 4).forEach((category) => {
-        // console.log(category);
-        // html elementi oluşturma
-        const categoryDiv = document.createElement("div");
-        // Category divine class ekleme
-        categoryDiv.classList.add("category");
-
-        // category div içerisindeki elemanları ekleme
-        categoryDiv.innerHTML = `<img src="${category.image}"
-        alt="Kategori">
-        <span>${category.name}</span>
-        `;
-
-        // kategorileri categories divine ekleme
-        categoryList.appendChild(categoryDiv);
-
-    });
-
+  // kategoriler dizisindeki herbir obje için ekran kart basma
+  categories.forEach((category) => {
+    // 1- bir div oluşturma
+    const categoryDiv = document.createElement('div');
+    // 2- dive class ekleme
+    categoryDiv.classList.add('category');
+    // 3- divin içeriğini belirleme
+    categoryDiv.innerHTML = `
+     <img src=${category.image} />
+     <span>${category.name}</span>
+    `;
+    // 4- elemanı htmldeki categories divine ekleme
+    categoryList.appendChild(categoryDiv);
+  });
 }
 
-// Ürünleri ekrana bastırma
+/*
+ * 1- <div></div>
+ * 2- <div className="category"></div>
+ * 3- <div className="category">
+ *     <img src="https://picsum.photos/640/640?r=1249" />
+ *     <span>Elektronik</span>
+ *    </div>
+ * 4- Oluşturduğumuz bu categoryDiv'i (node) html'e gönderme
+ */
+
 export function renderProducts(products) {
-    products.slice(0, 39).forEach((product) => {
-        console.log(product);
-        const productDiv = document.createElement("div");
-        productDiv.className = "product";
-        productDiv.innerHTML = `<img src="${product.images}"
-        alt="Ürün">
+  products
+    .slice(0, 40)
+    // dizideki her bir obje için çalışır
+    .forEach((product) => {
+      // div oluşturma
+      const productCard = document.createElement('div');
+      // gerekli class atamasını yapma
+      productCard.className = 'product';
+      // kartın içeriğini belirleme
+      productCard.innerHTML = `
+            <img src=${product.images[0]} />
             <p>${product.title}</p>
             <p>${product.category.name}</p>
             <div class="product-info">
-            <p>777 tl</p>
-            <button>Sepete ekle</button>
-         </div>`;
-
-         productList.appendChild(productDiv);
+              <p>${product.price} $</p>
+              <button id="add-btn" data-id=${product.id}>Sepete Ekle</button>
+            </div>      
+      `;
+      //   elemanı html'e gönderme
+      productList.appendChild(productCard);
     });
-
-
 }
+
+// ürünü ekran basma fonksiyonu
+export function renderBasketItem(product) {
+  const basketItem = document.createElement('div');
+
+  basketItem.classList.add('list-item');
+
+  basketItem.innerHTML = `
+   <img src=${product.images[0]} />
+   <h2>${product.title}</h2>
+   <h2>${product.price}</h2>
+   <p>Miktar: ${product.amount}</p>
+   <button id="del-button" data-id=${product.id}>sil</button>
+  `;
+
+  basketList.appendChild(basketItem);
+}
+
+// Alternatif yöntem:
+// document.body.addEventListener('click', (event) => {
+//   if (event.target === sepetBtn) {
+//     modal.classList.add('active');
+//   } else if (event.target === closeBtn) {
+//     modal.classList.remove('active');
+//   }
+// });
